9f012827a8f557ee5ac0f98b0a5ed3119330f065
4rch1tT <architanirudhan123@gmail.com>
2026-01-10 14:59:57 +0000

    added location store

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -1,13 +1,13 @@
 import { WindowControls } from "#components";
 import WindowWrapper from "#hoc/WindowWrapper";
 import { Download } from "lucide-react";
-import {Document,Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
 ).toString();
 
 const Resume = () => {
@@ -22,10 +22,10 @@ const Resume = () => {
           className="cursor-pointer"
           title="Download resume"
         >
-          <Download className="icon"/>
+          <Download className="icon" />
         </a>
       </div>
-      <Document file="files/resume.pdf" >
+      <Document file="files/resume.pdf">
         <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
       </Document>
     </>
