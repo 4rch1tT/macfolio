226b0e21e257b0da4d90678cc10f93420b0ae293
4rch1tT <architanirudhan123@gmail.com>
2026-01-10 18:10:24 +0000

    added finder logic

--- src/store/location.js
+++ src/store/location.js
@@ -1,15 +1,18 @@
-import { locations } from "#constants"
-import { create } from "zustand"
-import { immer } from "zustand/middleware/immer"
+import { locations } from "#constants";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 const DEFAULT_LOCATION = locations.work;
 
-const useLocationStore = create(immer((set)=>({
- activeLocation: DEFAULT_LOCATION,
+const useLocationStore = create(
+  immer((set) => ({
+    activeLocation: DEFAULT_LOCATION,
 
- setActiveLocation: (location=null)=> set((state)=>{
-  state.activeLocation = location;
- })
-})))
+    setActiveLocation: (location = null) =>
+      set((state) => {
+        state.activeLocation = location;
+      }),
+  }))
+);
 
-export default useLocationStore
+export default useLocationStore;
